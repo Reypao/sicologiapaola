b9d87f8ee888fbd4bf060a54313ba2d74c21d3cb
Efrey Guzman <efrey5guzman@gmail.com>
2025-10-10 18:08:07 -0600

    update17

--- scripts/podcast-detail.js
+++ scripts/podcast-detail.js
@@ -4,3 +4,16 @@ console.log("id podcast: ",id)
 
 const episodeselect=document.getElementById("ep"+id);
 episodeselect.classList.remove("d-none")
+
+//--botton share--//
+
+const currentUrl = window.location.href; // URL completa actual
+const text = encodeURIComponent("Escucha este episodio del podcast de Paola Guzmán");
+
+const facebookBtn = document.getElementById("share-fb");
+const twitterBtn = document.getElementById("share-tw");
+
+if (facebookBtn && twitterBtn) {
+  facebookBtn.href =`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(currentUrl)}`;
+  twitterBtn.href = `https://twitter.com/intent/tweet?url=${encodeURIComponent(currentUrl)}&text=${text}`;
+}

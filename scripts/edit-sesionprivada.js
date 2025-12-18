3eac3755f0b0918c0741774d1855d2ad4692d370
Efrey Guzman <efrey5guzman@gmail.com>
2025-12-18 05:06:44 -0700

    update

--- scripts/edit-sesionprivada.js
+++ scripts/edit-sesionprivada.js
@@ -7,7 +7,7 @@ document.addEventListener("click", function (e) {
   document.getElementById("sesion-cliente").value = fila.dataset.cliente;
   document.getElementById("sesion-fecha").value = fila.dataset.fecha;
   document.getElementById("sesion-hora").value = fila.dataset.hora;
-  document.getElementById("sesion-estado").value = fila.dataset.estado;
+  document.getElementById("sesion_estado").value = fila.dataset.estado;
   document.getElementById("sesion-comentario").value = fila.dataset.comentario;
 
   const modal = new bootstrap.Modal(
